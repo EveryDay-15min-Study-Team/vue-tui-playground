e725ace4bf800dc947c07cf9ba88132fc8c1fd40
dev.juyoung <dev.juyoung@gmail.com>
2018-02-24 16:00:58 +0900

    * tui.editor에 필요한 css 모듈 로드 및 style 수정.

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,11 @@ export default {
 <style lang="scss">
 // normalize.css load.
 @import url("../node_modules/normalize.css/normalize.css");
+// tui.editor styles.
+@import url('../node_modules/codemirror/lib/codemirror.css');
+@import url('../node_modules/tui-editor/dist/tui-editor.css');
+@import url('../node_modules/tui-editor/dist/tui-editor-contents.css');
+@import url('../node_modules/highlight.js/styles/github.css');
 
 %app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -33,8 +38,19 @@ body {
 
 #app {
   @extend %app;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+
+#app img {
+  margin: 0 auto;
+}
+
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 840px;
+  flex: 1;
+  margin: 0 auto;
 }
 </style>
