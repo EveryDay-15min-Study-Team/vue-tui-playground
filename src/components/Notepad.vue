1c9bc304af89409ce7b1d4e133442067ceff12d8
dev.juyoung <dev.juyoung@gmail.com>
2018-02-24 15:59:58 +0900

    * 실제 tui.editor를 실행하기 위한 component 정의.

---
+++ src/components/Notepad.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-wrapper">
+    <div id="editor"></div>
+  </div>
+</template>
+
+<script>
+import Editor from 'tui-editor';
+
+export default {
+  name: 'Notepad',
+  mounted() {
+    this.editor = new Editor({
+      el: document.querySelector('#editor'),
+      initialEditType: 'markdown',
+      previewStyle: 'vertical',
+    });
+  },
+  data() {
+    return {
+      editor: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#editor {
+  flex: 1;
+  margin-bottom: 60px;
+}
+</style>
