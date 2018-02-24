ee7a2544aafdd119585abbdee4a21cbed93eee12
dev.juyoung <dev.juyoung@gmail.com>
2018-02-24 15:06:53 +0900

    * style에 scss 적용.

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,28 @@ export default {
 };
 </script>
 
-<style>
-#app {
+<style lang="scss">
+// normalize.css load.
+@import url("../node_modules/normalize.css/normalize.css");
+
+%app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100vw;
+  height: 100vh;
+}
+
+html {
+  @extend %app;
+}
+
+body {
+  @extend %app;
+}
+
+#app {
+  @extend %app;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
